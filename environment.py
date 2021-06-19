76e4f1e057c3ca6de3096cc421471abdbb3de6fe
mrussell481 <84339257+mrussell481@users.noreply.github.com>
2021-06-18 21:59:07 -0400

    initial upload

---
+++ environment.py
@@ -0,0 +1,5 @@
+# Features do the following:
+# Login tests both manager and member logins
+# Create makes two new requests as a member
+# Judge approves one request and denies the other as a manager
+# View selects three different requests as a manager
